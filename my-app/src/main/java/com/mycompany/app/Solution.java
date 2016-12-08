94f542b4eb39ea5d0dd4d21351466c455d9dbc50
Dustin Brown <dbrown@jamasoftware.com>
2016-12-07 22:59:29 -0800

    first commit for supermarket

---
+++ my-app/src/main/java/com/mycompany/app/Solution.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Solution {
+
+    public static int solveSuperMarketQueue(int[] customers, int n) {
+        Integer time = 0;
+//        for (int i=0 ; i <= customers.size() ; i +=n) {
+//            for (int k=k; k < n; k++) {
+//                time += customers.get(k);
+//            }
+//        }
+        return time;
+    }
+
+}
